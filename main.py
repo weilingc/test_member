6b30101a2e627c6a1b67c4470f710b07bacd45d5
beta lin <60283086+linbeta@users.noreply.github.com>
2021-10-22 15:00:29 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+from replit import db
+import gui
+import test_db
+# import os
+
+
+# 變數設定
+
+''''
+member_num  編號
+user_eamil 使用者郵件(已在資料庫) 
+user_psd 使用者密碼
+last_name 姓
+first_name 名
+new_email 新的郵件(註冊時)
+new_user
+test_db 資料庫名稱
+'''
+
+
+data = test_db.db[memeber_num]
+print(data)
+
+# 測試replit database
+
+
+
+
+# Function設定
+def check_new_email(user_mail):
+    
+    return "OK"
+
+
+def check_email_form(new_email):
+    pass
+
+
+
+
+
+# test = check_new_email(gui.new_email)
+# print(test)
---
+++ test_db.py
@@ -0,0 +1,14 @@
+# 資料庫字典
+db = {
+   "001": {
+       "email": "abc@abc.com",
+       "password": "Abc123",
+       "first_name": "Beta",
+       "last_name": "Lin"
+   } ,
+   "002": {
+       "email": "123@123.com",
+       
+   }
+
+}
